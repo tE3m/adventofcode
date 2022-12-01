befaeca00d15c7c2f020eef588db046ea359c047
tE3m <tarek.jannis.03.tm@gmail.com>
2022-12-01 08:27:15 +0100

    First day completed

---
+++ day-01/task2.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+    with open("input.txt") as file:
+        lines = [int(line.strip()) if line != "\n" else -1 for line in file.readlines()]
+    elves = [0]
+    for line in lines:
+        if line == -1:
+            elves.append(0)
+        else:
+            elves[-1] += line
+    print(sum(sorted(elves)[:-4:-1]))
--- day-7/task-1.py
+++
@@ -1,8 +0,0 @@
-def main():
-    positions: list[int] = [int(position) for position in open("input.txt").readline().split(",")]
-    fuelCosts = {possiblePosition: sum(abs(position-possiblePosition) for position in positions) for possiblePosition in range(min(positions), max(positions)+1)}
-    print(min(fuelCosts.values()))
-
-
-if __name__ == '__main__':
-    main()
